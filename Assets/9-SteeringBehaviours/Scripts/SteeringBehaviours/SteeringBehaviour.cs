7dea3826a47c71ad49b9d9d8c693024309b2c9bc
Timtam2724 <timfuller@hotmail.com>
2017-10-12 12:06:35 +1100

    Added steering behaviours

---
+++ Assets/9-SteeringBehaviours/Scripts/SteeringBehaviours/SteeringBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI
+{
+    [RequireComponent(typeof(AIAgent))]
+    public class SteeringBehaviour : MonoBehaviour
+    {
+        public float weighting = 7.5f;
+        public Vector3 force;
+        public AIAgent owner;
+
+        protected virtual void Awake()
+        {
+            owner = GetComponent<AIAgent>();
+        }
+        public virtual Vector3 GetForce()
+        {
+            return Vector3.zero;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
