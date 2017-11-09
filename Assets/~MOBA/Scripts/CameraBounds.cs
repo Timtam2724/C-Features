b37f88ce88cbfdf2f3311895fb89101f71c1fe8f
Timtam2724 <timfuller@hotmail.com>
2017-11-09 11:59:52 +1100

    Skeleton archer

---
+++ Assets/~MOBA/Scripts/CameraBounds.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MOBA
+{
+    public class CameraBounds : MonoBehaviour
+    {
+        public Vector3 size = new Vector3(50f, 0f, 90f);
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(transform.position, size);
+        }
+
+        // Adjusts the position to contrain it within size
+        public Vector3 GetAdjustedPos(Vector3 incomingPos)
+        {
+            Vector3 pos = transform.position;
+            Vector3 halfSize = size * 0.5f;
+
+            // Is incomingPos outside the positive Z?
+            if(incomingPos.z > pos.z + halfSize.z)
+            {
+                incomingPos.z = pos.z + halfSize.z;
+            }
+            // Is incoming outside the negative Z?
+            if(incomingPos.z < pos.z - halfSize.z)
+            {
+                incomingPos.z = pos.z - halfSize.z;
+            }
+            // Is incoming outside the positive X?
+            if(incomingPos.x > pos.x + halfSize.x)
+            {
+                incomingPos.x = pos.x + halfSize.x;
+            }
+            // Is incoming outside the negative X?
+            if(incomingPos.x < pos.x - halfSize.x)
+            {
+                incomingPos.x = pos.x - halfSize.x;
+            }
+            // Is incoming outside the positive Y?
+            if (incomingPos.y > pos.y + halfSize.y)
+            {
+                incomingPos.y = pos.y + halfSize.y;
+            }
+            // Is incoming outside the negative Y?
+            if (incomingPos.y < pos.y - halfSize.y)
+            {
+                incomingPos.y = pos.y - halfSize.y;
+            }
+            // Returns the adjusted incomingPos
+            return incomingPos;
+        }       
+    }
+}
